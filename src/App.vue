8932bfe15d0cfdd1c3c6e8a1f2ffef0bc4ce19b2
DanielaVenturi <danielaventuri14@gmail.com>
2023-05-14 16:14:34 -0300

    final

--- src/App.vue
+++ src/App.vue
@@ -1,93 +1,94 @@
 <script setup>
 import { ref } from 'vue'
 
+const mostrar = ref(false)
 const listaCompras1 = ref([
   {
     id: 1,
     nome: 'Chaveiro do Cascão-Turma Da Mônica',
     preco: 7.9,
     quantidade: 0,
-    img: `chavCascao.webp`
+    img: `https://http2.mlstatic.com/D_NQ_NP_782075-MLB41375108462_042020-O.jpg`
   },
   {
     id: 2,
     nome: 'Chaveiro do Cebolinha-Turma Da Mônica',
     preco: 7.9,
     quantidade: 0,
-    img: 'chaveiroCebolinha.webp'
+    img: 'https://www.lojapassatempo.com.br/galeria/produtos/gra/1201_chaveiro-de-metal-turma-da-monica-cebolinha_1966848972.jpg'
   },
   {
     id: 3,
     nome: 'Chaveiro da Magali-Turma Da Mônica',
-    preco: 7.9,
+    preco: 8.9,
     quantidade: 0,
-    img: 'chaveiroMagali.jpg'
+    img: 'https://a-static.mlcdn.com.br/800x560/chaveiro-de-metal-magali-5cm-turma-da-monica-tasco/livepointgames/14983346124/2c5961e188096579a512ca563a6c3041.jpg'
   },
   {
     id: 4,
     nome: 'Chaveiro da Mônica-Turma da Mônica',
-    preco: 7.9,
+    preco: 8.9,
     quantidade: 0,
-    img: 'ChaveiroMonica.jpg'
+    img: 'https://lojaminasdepresentes.com.br/media/catalog/product/cache/1/thumbnail/1200x1400/9df78eab33525d08d6e5fb8d27136e95/m/d/mdy839a-m1_2_.jpg'
   },
   {
     id: 5,
     nome: 'Chaveiro do Mingau-Turma da Mônica',
     preco: 7.9,
     quantidade: 0,
-    img: 'ChavMingau.webp'
+    img: 'https://a-static.mlcdn.com.br/1500x1500/chaveiro-de-metal-turma-da-monica-mingau-sentado-tasco-import/livepointgames/7442083215/5c7308998df791b5da4168afe98f618f.jpg'
   },
   {
     id: 6,
     nome: 'Chaveiro do Tasco-Turma Da Mônica',
     preco: 7.9,
     quantidade: 0,
-    img: 'Chavtasco.jpg'
+    img: 'https://imgs.casasbahia.com.br/1509127813/1xg.jpg?imwidth=500'
   }
 ])
 
 const listaCompras2 = ref([
   {
     id: 1,
-    nome: 'Moana',
-    preco: 7.9,
+    nome: 'Chaveiro Elsa-Frozen',
+    preco: 9.9,
     quantidade: 0,
-    img: `ChavMoanaPerso.jpg`
+    img: `https://atacadominasdepresentes.com.br/media/catalog/product/cache/1/thumbnail/1200x1400/9df78eab33525d08d6e5fb8d27136e95/f/z/fz-dy839a-1-d.jpg`
   },
   {
     id: 2,
-    nome: '',
-    preco: 7.9,
+    nome: 'Chaveiro coraçao irmãs de arendel-Frozen',
+    preco: 11.9,
     quantidade: 0,
-    img: 'chaveiroCebolinha.webp'
+    img: 'https://nudelmania.vteximg.com.br/arquivos/ids/184996-1000-1000/Chaveiro-Frozen-Disney---Espuma---unidade.jpg?v=637435848667170000'
   },
   {
     id: 3,
-    nome: 'Chaveiro da Magali-Turma Da Mônica',
+    nome: 'Chaveiro do Olaf-Frozen',
     preco: 7.9,
     quantidade: 0,
-    img: 'chaveiroMagali.jpg'
+    img: 'https://www.merchandisingplaza.com.br/228858/2/Chaveiros-Frozen-Chaveiro-Frozen-228858-l.jpg'
   },
   {
     id: 4,
-    nome: 'Chaveiro da Mônica-Turma da Mônica',
-    preco: 7.9,
+    nome: 'Chaveiro BFF-Frozen',
+    preco: 11.9,
     quantidade: 0,
-    img: 'ChaveiroMonica.jpg'
+    img: 'https://lojaminasdepresentes.com.br/media/catalog/product/cache/1/thumbnail/1200x1400/9df78eab33525d08d6e5fb8d27136e95/f/z/fz-dy839a-5-d_3_.jpg'
   },
   {
     id: 5,
-    nome: 'Chaveiro do Mingau-Turma da Mônica',
+    nome: 'Chaveiro da Ana-Frozen',
     preco: 7.9,
     quantidade: 0,
-    img: 'ChavMingau.webp'
+    img: 'https://atacadominasdepresentes.com.br/media/catalog/product/cache/1/thumbnail/1200x1400/9df78eab33525d08d6e5fb8d27136e95/f/z/fz-dy839a-2-d.jpg'
   },
   {
     id: 6,
-    nome: 'Chaveiro do Tasco-Turma Da Mônica',
-    preco: 7.9,
+    nome: 'Chaveiro Personalizado Elsa-Frozen',
+    preco: 23.9,
     quantidade: 0,
-    img: 'Chavtasco.jpg'
+    img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS18_WEd3DeNwR9WLfMjStyeKBS1XW0CqBpCQ&usqp=CAU'
   }
 ])
 
@@ -141,15 +142,61 @@ function adicionar(index, lista) {
   }
 }
 
-function remover(index) {
-  carrinhos.value.items.pop({
-    ...listaCompras1.value[index],
-    preco: listaCompras1.value[index].quantidade * listaCompras1.value[index].preco
-  })
+function remover(index){
+  const item = carrinhos.value.items.findIndex(items => items > index);
+  carrinhos.value.items.splice(item, 1);
+  
+  
 }
+
 </script>
 
 <template>
+
+
+<nav class="navbar navbar-expand-lg bg-body-tertiary " >
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">CHAV.Dani</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+
+        <li class="nav-item">
+          <a class="nav-link disabled">Loja De Chaveiros</a>
+        </li>
+      </ul>
+      <div class="">
+    <button class="btn btn-outline-secondary" @click="mostrar = true">Carrinho</button>
+  </div>
+    </div>
+  </div>
+</nav>
+
+<div class="carrinho" v-if="mostrar">
+    <h1>Seu Carrinho</h1>
+   
+    <button class="btn btn-outline-secondary"  @click="mostrar = false">Fechar Carrinho</button>
+   
+    <div class="item" v-for="item in carrinhos.items" :key="item.id">
+      <p>Item: {{ item.nome }}</p>
+      <p>Preco: {{ item.preco.toFixed(2) }}</p>
+      <p>ID: {{ item.id }}</p>
+      <p>Quantidade: {{ item.quantidade }}</p>
+      <img :src="item.img" />
+      <div >
+        <button class="btn-close" aria-label="Close" @click="remover"></button>
+    </div>
+  </div>
+    <h1>Total:{{ carrinhos.total.toFixed(2) }}</h1>
+  </div>
+
+  <hr>
+
+<div class="">
+  </div>
+<h1>TURMA DA MÔNICA</h1>
   <div class="box">
     <div class="item" v-for="(item, index) in listaCompras1" :key="item.id">
       <p>Item: {{ item.nome }}</p>
@@ -160,13 +207,15 @@ function remover(index) {
       <img :src="item.img" />
 
       <div class="botao">
-        <button @click="decrementar(index, 1)">-</button>
-        <button @click="incrementar(index, 1)">+</button> <br />
-        <button @click="adicionar(index, 1)">Adicionar</button>
+        <button class="btn btn-outline-secondary" @click="decrementar(index, 1)">-</button>
+        <button class="btn btn-outline-secondary" @click="incrementar(index, 1)">+</button> <br />
+        <button class="btn btn-outline-secondary" @click="adicionar(index, 1)" >Adicionar</button>
+        
+        
       </div>
     </div>
   </div>
-
+<h1>FROZEN</h1>
   <div class="box">
     <div class="item" v-for="(item, index) in listaCompras2" :key="item.id">
       <p>Item: {{ item.nome }}</p>
@@ -177,58 +226,51 @@ function remover(index) {
       <img :src="item.img" />
 
       <div class="botao">
-        <button @click="decrementar(index, 2)">-</button>
-        <button @click="incrementar(index, 2)">+</button> <br />
-        <button @click="adicionar(index,2 )">Adicionar</button>
+        <button class="btn btn-outline-secondary" @click="decrementar(index, 2)">-</button>
+        <button class="btn btn-outline-secondary" @click="incrementar(index, 2)">+</button> <br />
+        <button class="btn btn-outline-secondary" @click="adicionar(index,2 )">Adicionar</button>
       </div>
     </div>
+   
   </div>
-
+  
   
 
-  <div class="carrinho">
-    <div class="item" v-for="item in carrinhos.items">
-      <p>Item: {{ item.nome }}</p>
-      <p>Preco: {{ item.preco.toFixed(2) }}</p>
-      <p>ID: {{ item.id }}</p>
-      <p>Quantidade: {{ item.quantidade }}</p>
-      <img :src="item.img" />
-      <div class="botao"><button @click="remover">remover</button>
-    </div></div>
-    <p>Total:{{ carrinhos.total.toFixed(2) }}</p>
-  </div>
+  
 </template>
 
 <style scoped>
 .box {
   border-style: double;
   display: grid;
-  grid-template-columns: 400px 400px 400px;
+  grid-template-columns: 400px 400px ;
   grid-template-rows: 400px 400´x 400px ;
+  
+ 
+
+  
+  
 }
 .carrinho{
 
-  border-style: double;
-  display: grid;
-  grid-template-columns: 400px 400px 400px;
-  grid-template-rows: 400px 400px 400px ;
-  border-style: groove;
-  
 
 
 }
 .item {
   margin-top: 50px;
   border-style: groove;
-  margin-right: 25px;
-  margin-left: 25px;
+  background-color: rgba(138, 68, 204, 0.273);
   align-items: center;
   margin-bottom: 50px;
+  text-align: center;
+ 
   
 }
 
-p {
+h1{
   text-align: center;
+  margin-top: 25px;
+  margin-bottom: 25px;
 }
 
 .botao {
@@ -236,6 +278,7 @@ p {
   border-style: groove;
   margin-left: 75px;
   margin-right: 75px;
+  background-color: rgba(39, 9, 67, 0.273);
 }
 img {
   border-style: groove;
